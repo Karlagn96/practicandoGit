6fc23809871671becb3ee02821f56a3edc051a91
Karla <karlaivonnegn@gmail.com>
2021-02-21 15:47:37 -0600

    se agrego el archivo index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('hola mundito')
+
+let queso='mi primera variable'
+console.log(queso)
+
+queso= 'lloro mucho ayuda'
+console.log(queso)
+
+const miedo= {}
+
+const miedO= {
+    cebolla: 'bannaa de posho',
+    pizza: 1234,
+    caballo: 'abeja'
+}
+
+console.log(miedO.cebolla, miedO.caballo)
+
+const arr= [1, 4, 'conejo', 'lagarto', 'pieza']
+
+arr.push(234)
+
+console.log(arr)
