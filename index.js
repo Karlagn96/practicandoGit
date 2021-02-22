47a70c36de3929f1fba52384e1ae5a687403065e
Karla <karlaivonnegn@gmail.com>
2021-02-21 21:13:44 -0600

    añadi mas informacion sobre operaciones que puedo aplicar

--- index.js
+++ index.js
@@ -14,10 +14,77 @@ const miedO= {
     caballo: 'abeja'
 }
 
-console.log(miedO.cebolla, miedO.caballo)
+//console.log(miedO.cebolla, miedO.caballo)
 
 const arr= [1, 4, 'conejo', 'lagarto', 'pieza']
 
 arr.push(234)
 
-console.log(arr)
+//console.log(arr)
+
+const suma = 1+2
+const resta = 10-5
+const dividir= 5/10
+const multiplicar= 5*4
+const modulo = 10%3
+
+//console.log(suma, resta, dividir, multiplicar, modulo)
+
+//se cambia a let ya que si utilizamos const el valor no puede cambiar
+let incremento = 10
+//incremento++
+//incremento++
+//incremento++
+//incremento++
+//incremento++
+
+//incremento--
+//incremento--
+//incremento--
+
+//hay para todos los operadores suma, resta, mult y div
+incremento+=4
+//console.log(incremento)
+
+//operadores de comparacion
+
+//igualdad estricta
+const igualisimo= 5===8
+//igualdad no estricta
+const igualisimo2= 5== '5'
+
+const igualisimo3= 5 < 6
+const igualisimo4= 5 < 5
+const igualisimo5= 5 > 6
+const igualisimo6= 5 > 4
+const igualisimo7= 5 <= 5
+const igualisimo8= 5 <= 6
+const igualisimo9= 5 >= 5
+const igualisimo10= 5 >= 6
+
+const igualisimo11= 5 !== 6
+const igualisimo12= 5 != '5'
+//console.log(igualisimo2)
+
+//or ||, and &&, not !!
+
+//hasta que encuentre true lo reflejara
+const resultadOR = true || false
+
+//hasta que encuentre false es lo que reflejara
+const resultAnd = true && false
+
+const resultNot = !true
+
+console.log(resultNot)
+
+
+
+
+
+
+
+
+
+
+
